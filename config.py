5551dac61bd5e4c8da664cb907ccaeac4bf75c30
mapyJJJ <wx_f2b793c05d3c4c4f95634c0890ac6918@git.code.tencent.com>
2019-04-23 10:53:58 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+# Redis数据库地址
+REDIS_HOST = 'localhost'
+
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，请设为空
+
+#默认设置，无需更改
+REDIS_DOMAIN = '*'
+REDIS_NAME = '*'
+
+#数据库标号
+REDIS_DB=3
+
+#api接口配置
+API_HOST='0.0.0.0'
+API_PORT=5000
+
+#运行平台
+PLATFORM='windwos'
+
+#默认开启cookies进程和api进程
+GENERATOR_PROCESS=True
+API_PROCESS=True
